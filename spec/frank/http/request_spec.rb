14615c9f852315bbd88f1fb73453b48a8225da9d
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-05-31 07:03:32 -0400

    fix request spec

--- spec/frank/http/request_spec.rb
+++ spec/frank/http/request_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 module Frank::HTTP
   describe Request do
-    it "raises if a trace request contains body" do
-      request = Request.new("TRACE")
-      expect { request.body = 'body' }.to raise_error(TraceRequestBody)
+    describe "#body" do
+      it "sets body for all acceptable methods" do
+        ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS"].each do |method|
+          request = Request.new(method)
+          request.body = 'body'
+          request.body.should eq('body')
+        end
+      end
+
+      it "raises if a trace request contains body" do
+        request = Request.new("TRACE")
+        expect { request.body = 'body' }.to raise_error(TraceRequestBody)
+      end
     end
   end
 end
